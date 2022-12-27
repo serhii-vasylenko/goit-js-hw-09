2da41cb12ef25a292f38d610e2945b583e2c1413
Jerrssey <fankton@gmail.com>
2022-12-27 17:48:56 +0200

    Task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+let timerId = null;
+refs.stopBtn.disabled = true;
 
+refs.startBtn.addEventListener('click', enableBodyRandomBgColor);
+refs.stopBtn.addEventListener('click', disableBodyRandomBgColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function enableBodyRandomBgColor() {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  timerId = setInterval(
+    () => (document.body.style.backgroundColor = `${getRandomHexColor()}`),
+    2000
+  );
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function disableBodyRandomBgColor() {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
